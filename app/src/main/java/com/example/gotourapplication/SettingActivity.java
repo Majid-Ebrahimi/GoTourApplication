fd4d88cd6c3596d21a40ff870ad297d607286d86
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2022-11-07 15:58:05 +0330

    Initial commit

--- app/src/main/java/com/example/gotourapplication/SettingActivity.java
+++ app/src/main/java/com/example/gotourapplication/SettingActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.gotourapplication.databinding.ActivitySettingBinding;
 
@@ -17,11 +19,14 @@ public class SettingActivity extends AppCompatActivity {
         binding = ActivitySettingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.HelloTextView.setOnClickListener(view ->{
+//back button animation and set onClick for direct to main activity
+        Animation shake = AnimationUtils.loadAnimation(this,R.anim.shake_animation);
+        binding.MainBackButton.setOnClickListener(view ->{
             Intent intent = new Intent(SettingActivity.this,MainActivity.class);
             startActivity(intent);
             overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
-
+            binding.MainBackButton.startAnimation(shake);
         });
     }
+//
 }
