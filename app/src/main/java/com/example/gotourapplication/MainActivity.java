ad922a606714112b409a506e34dea8eec93fe520
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2022-12-03 16:49:36 +0330

    Initial commit

--- app/src/main/java/com/example/gotourapplication/MainActivity.java
+++ app/src/main/java/com/example/gotourapplication/MainActivity.java
@@ -2,23 +2,92 @@ package com.example.gotourapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.gotourapplication.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private ActivityMainBinding binding;
+//Recycler VIEW
+    RecyclerView rv;
+    ArrayList<String> dataSource;
+    ArrayList<Integer> dataSource2;
+    ArrayList<String> dataSource3;
+    ArrayList<String> dataSource4;
+
+    LinearLayoutManager linearLayoutManager;
+    MyRvAdapter myRvAdapter;
+
+    class MyRvAdapter extends RecyclerView.Adapter<MyRvAdapter.MyHolder> {
+        ArrayList<String> data;
+        ArrayList<Integer> data2;
+        ArrayList<String> data3;
+        ArrayList<String> data4;
+
+        public MyRvAdapter(ArrayList<String> data,ArrayList<Integer> data2,ArrayList<String> data3, ArrayList<String> data4) {
+            this.data = data;
+            this.data2 = data2;
+            this.data3 = data3;
+            this.data4 = data4;
+        }
+
+        @NonNull
+        @Override
+        public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.rv_item, parent, false);
+            return new MyHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+            holder.tvTitle.setText(data.get(position));
+            holder.imageRV.setImageResource(data2.get(position));
+            holder.price.setText(data3.get(position));
+            holder.location.setText(data4.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class MyHolder extends RecyclerView.ViewHolder {
+            TextView tvTitle;
+            ImageView imageRV;
+            TextView price;
+            TextView location;
+            public MyHolder(@NonNull View itemView) {
+                super(itemView);
+                tvTitle = itemView.findViewById(R.id.tvTitle);
+                imageRV = itemView.findViewById(R.id.imageRV);
+                price = itemView.findViewById(R.id.price);
+                location = itemView.findViewById(R.id.location);
+            }
+        }
+
+    }
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +96,66 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 //
+        //Recycler View
+        rv = findViewById(R.id.horizontalRv);
+
+        //Setting the data source
+        dataSource = new ArrayList<>();
+        dataSource.add(getString(R.string.image_title_string_1));
+        dataSource.add(getString(R.string.image_title_string_2));
+        dataSource.add(getString(R.string.image_title_string_3));
+        dataSource.add(getString(R.string.image_title_string_4));
+        dataSource.add(getString(R.string.image_title_string_5));
+        dataSource.add(getString(R.string.image_title_string_6));
+        dataSource.add(getString(R.string.image_title_string_7));
+        dataSource.add(getString(R.string.image_title_string_8));
+        dataSource.add(getString(R.string.image_title_string_9));
+        dataSource.add(getString(R.string.image_title_string_10));
+
+
+        dataSource2 = new ArrayList<>();
+        dataSource2.add(R.drawable.image_1);
+        dataSource2.add(R.drawable.image_2);
+        dataSource2.add(R.drawable.image_3);
+        dataSource2.add(R.drawable.image_4);
+        dataSource2.add(R.drawable.image_5);
+        dataSource2.add(R.drawable.image_6);
+        dataSource2.add(R.drawable.image_7);
+        dataSource2.add(R.drawable.image_8);
+        dataSource2.add(R.drawable.image_9);
+        dataSource2.add(R.drawable.image_10);
+
+
+        dataSource3 = new ArrayList<>();
+        dataSource3.add(getString(R.string.image_prices_1));
+        dataSource3.add(getString(R.string.image_prices_2));
+        dataSource3.add(getString(R.string.image_prices_3));
+        dataSource3.add(getString(R.string.image_prices_4));
+        dataSource3.add(getString(R.string.image_prices_5));
+        dataSource3.add(getString(R.string.image_prices_6));
+        dataSource3.add(getString(R.string.image_prices_7));
+        dataSource3.add(getString(R.string.image_prices_8));
+        dataSource3.add(getString(R.string.image_prices_9));
+        dataSource3.add(getString(R.string.image_prices_10));
+
+        dataSource4 = new ArrayList<>();
+        dataSource4.add(getString(R.string.image_location_string_1));
+        dataSource4.add(getString(R.string.image_location_string_2));
+        dataSource4.add(getString(R.string.image_location_string_3));
+        dataSource4.add(getString(R.string.image_location_string_4));
+        dataSource4.add(getString(R.string.image_location_string_5));
+        dataSource4.add(getString(R.string.image_location_string_6));
+        dataSource4.add(getString(R.string.image_location_string_7));
+        dataSource4.add(getString(R.string.image_location_string_8));
+        dataSource4.add(getString(R.string.image_location_string_9));
+        dataSource4.add(getString(R.string.image_location_string_10));
+
+        linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        myRvAdapter = new MyRvAdapter(dataSource,dataSource2,dataSource3,dataSource4);
+        rv.setLayoutManager(linearLayoutManager);
+        rv.setAdapter(myRvAdapter);
+        //
+
 //menu button animation and set onClick for direct to setting activity
         Animation shake = AnimationUtils.loadAnimation(this,R.anim.shake_animation);
         binding.MenuButton.setOnClickListener(view ->{
@@ -110,3 +239,4 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 //
 }
+
